081c1b60be4244e23d416282b872c31398467023
AlbertT4 <84191428+ATedeschi247@users.noreply.github.com>
2022-10-10 13:29:47 +0000

    continued with 6, but didn't finish though

---
+++ docObjModel.js
@@ -0,0 +1,71 @@
+// const para = document.querySelector('div.error'); //1st p tag is grabbed
+
+// console.log(para);
+
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
+
+// console.log(errors);
+
+// get element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get element by class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+// errors.forEach(function(error){
+//   console.log(error);
+// });
+
+// get elements by tag name
+// const paras = document.getElementsByTagName('p');
+
+// console.log(paras);
+// console.log(paras[1]);
+
+// const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome';
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(function(para) {
+//   console.log(para.innerText);
+//   para.innerText += ' new text';
+// });
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>This is a new h2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+people.forEach(function(person) {
+  content.innerHTML += `<p>${person}</p>`;
+});
+
+const link = document.querySelector('a');
+
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText = 'net ninja site';
+
+const mssg = document.querySelector('p');
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color: green');
+
+const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px');
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.margin = "";
